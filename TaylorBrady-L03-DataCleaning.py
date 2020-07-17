43db66eb87bad05230d6ecbe0205d34a2629ad44
Taylor Brady <taylorllippert@hotmail.com>
2020-07-17 08:14:03 -0700

    Changed outline and added assignment directions

---
+++ TaylorBrady-L03-DataCleaning.py
@@ -0,0 +1,43 @@
+#File: TaylorBrady-L03-DataCleaning.py
+#Description: 
+#Author: Taylor Brady
+#Date: 7/17/2020
+#Other comments: 
+    
+# =============================================================================
+# Create a new Python script that will run in the grader's environment using only spyder's run file button.  
+#Include the following:
+# 
+# Import statements for necessary package(s)
+# Create a numeric numpy array, named arr1, with at least 30 items that contains outliers
+# Create a numpy array that should have been numeric, named arr2. 
+#      arr2 contains improper non-numeric missing values, like "?"
+# Call the three functions with their appropriate arrays in your script
+# Comments explaining the code blocks
+# Summary comment block on how your dataset has been cleaned up
+# =============================================================================
+
+#Description: removes outliers in arr1
+#Pre: 
+#Post: 
+def remove_outlier():
+
+
+
+#Description: replaces outliers in arr1 with the arithmetic mean of the non-outliers
+#Pre: 
+#Post: 
+def replace_outlier():
+
+
+
+#Description: fills in the missing values in arr2 with the median of arr2
+#Pre: 
+#Post: 
+def fill_median():
+
+    
+    
+#Description: 
+if __name__ == "__main__":
+
