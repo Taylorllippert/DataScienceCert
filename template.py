a2ecf0e5850837f25e621f644e057b70cacc93b5
Taylor Brady <taylorllippert@hotmail.com>
2020-07-17 08:05:59 -0700

    Created template for file header

---
+++ template.py
@@ -0,0 +1,23 @@
+#File: TaylorBrady-<lesson number>-<assignment name>.py
+#Description: 
+#Author: Taylor Brady
+#Date: 7/17/2020
+#Other comments: 
+
+#Description: 
+#Pre: 
+#Post: 
+def my_name():
+
+
+
+#Description: 
+#Pre: 
+#Post: 
+def date_and_time():
+
+
+
+#Description: 
+if __name__ == "__main__":
+
